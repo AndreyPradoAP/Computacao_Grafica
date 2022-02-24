5aae3ceae540903f4b394918932e31cf8ec928a0
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-02-24 17:13:47 -0300

    Códigos Aula 23/02/2022

---
+++ Funcoes_e_Procedimentos/Funcao_ObjetoCorRGB.cs
@@ -0,0 +1,7 @@
+private Color CorRGB(byte r, byte g, byte b)
+{
+    Color cor = new Color();
+    cor = Color.FromArgb(r, g, b);
+    return cor;
+}
+        
---
+++ Funcoes_e_Procedimentos/Procedimento_DesenharPonto.cs
@@ -0,0 +1,5 @@
+private void desenharPonto(PaintEventArgs e, int x, int y, Color cor)
+{
+    Pen caneta = new Pen(cor, 0);
+    e.Graphics.DrawLine(caneta, x, y, x+1, y);
+}
