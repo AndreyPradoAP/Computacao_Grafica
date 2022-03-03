539b1d2c554f34899e111662f5c7100b6cacefb6
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-03-03 11:35:14 -0300

    Update Procedimento_DesenharPonto.cs

--- Funcoes_e_Procedimentos/Procedimento_DesenharPonto.cs
+++ Funcoes_e_Procedimentos/Procedimento_DesenharPonto.cs
@@ -1,4 +1,4 @@
-private void desenharPonto(PaintEventArgs e, int x, int y, Color cor)
+public void desenharPonto(PaintEventArgs e, int x, int y, Color cor)
 {
     Pen caneta = new Pen(cor, 0);
     e.Graphics.DrawLine(caneta, x, y, x+1, y);
