153c793e98598393953b73af0554c102b44cd197
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-04-14 11:12:36 -0300

    Update Procedimento_DesenharRetangulo.cs

--- Funcoes_e_Procedimentos/Procedimento_DesenharRetangulo.cs
+++ Funcoes_e_Procedimentos/Procedimento_DesenharRetangulo.cs
@@ -1,4 +1,4 @@
-public void desenharRetangulo(PaintEventArgs e, Pen caneta, int espessuraReta, int x0, int y0, int largura, int altura)
+public void desenharRetangulo(PaintEventArgs e, Pen caneta, int x0, int y0, int largura, int altura)
         {
             e.Graphics.DrawRectangle(caneta, x0, y0, largura, altura);
         }
