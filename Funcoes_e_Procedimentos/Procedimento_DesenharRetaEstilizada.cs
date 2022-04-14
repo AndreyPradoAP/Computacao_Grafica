bfbebd3b917e4793debb67daf2fc610da9682476
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-04-14 11:04:34 -0300

    Update Procedimento_DesenharRetaEstilizada.cs

--- Funcoes_e_Procedimentos/Procedimento_DesenharRetaEstilizada.cs
+++ Funcoes_e_Procedimentos/Procedimento_DesenharRetaEstilizada.cs
@@ -1,6 +1,5 @@
-public void desenharRetaEstilizada(PaintEventArgs e, float[] estiloLinha, Color cor, int espessuraReta, int x0, int y0, int x, int y)
+public pen estilizarLinha(float[] estiloLinha, Color cor, int espessuraReta)
         {
             Pen caneta = new Pen(cor, espessuraReta);
             caneta.DashPattern = estiloLinha;
-            e.Graphics.DrawLine(caneta, x0, y0, x, y);
         }
