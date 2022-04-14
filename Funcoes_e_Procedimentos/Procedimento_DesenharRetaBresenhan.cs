b47acf333ff13370f1bbbd49968c6e4602feaef7
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-04-14 11:06:15 -0300

    Update Procedimento_DesenharRetaBresenhan.cs

--- Funcoes_e_Procedimentos/Procedimento_DesenharRetaBresenhan.cs
+++ Funcoes_e_Procedimentos/Procedimento_DesenharRetaBresenhan.cs
@@ -1,4 +1,3 @@
-public void desenharRetaBresenham(PaintEventArgs e, int x0, int y0, int x, int y, Color cor){
-    Pen caneta = new Pen(cor, 0);
+public void desenharRetaBresenham(PaintEventArgs e, Pen caneta, int x0, int y0, int x, int y){
     e.Graphics.DrawLine(caneta, x0, y0, x, y);
 }
