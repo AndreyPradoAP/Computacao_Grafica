8e339c5854afea921a0568c86e74389a435f831c
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-04-19 17:42:03 -0300

    Update Funcao_estilizarReta.cs

--- Funcoes_e_Procedimentos/Funcao_estilizarReta.cs
+++ Funcoes_e_Procedimentos/Funcao_estilizarReta.cs
@@ -1,5 +1,6 @@
-public pen estilizarLinha(float[] estiloLinha, Color cor, int espessuraReta)
+public Pen estilizarLinha(float[] estiloLinha, Color cor, int espessuraReta)
         {
             Pen caneta = new Pen(cor, espessuraReta);
             caneta.DashPattern = estiloLinha;
+            return caneta;
         }
