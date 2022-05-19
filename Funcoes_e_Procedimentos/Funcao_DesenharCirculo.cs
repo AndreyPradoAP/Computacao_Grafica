786faed2a9703c7acbed5199541490039543d435
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-05-19 12:10:15 -0300

    Update Funcao_DesenharCirculo.cs

--- Funcoes_e_Procedimentos/Funcao_DesenharCirculo.cs
+++ Funcoes_e_Procedimentos/Funcao_DesenharCirculo.cs
@@ -1,12 +1,14 @@
+/*
+        Tá em sentido horário
+*/
+
+
 public void desenharCirculo(PaintEventArgs e, Pen caneta, int Xcentro, int Ycentro, int raio, int anguloInicial, int anguloFinal ,Color cor)
         {
-            int x = 0;
-            int y = 0;
-
-            for (int c = anguloInicial; c >= anguloFinal; c--)
+            for (int c = anguloInicial; c <= anguloFinal; c++)
             {
-                x = (int) (Xcentro + raio * Math.Cos(c * Math.PI/180));
-                y = (int) (Ycentro + raio * Math.Sin(c * Math.PI / 180));
+                var x = (int) (Xcentro + raio * Math.Cos(c * Math.PI / 180));
+                var y = (int) (Ycentro + raio * Math.Sin(c * Math.PI / 180));
                 desenharPonto(e, caneta, x, y);
             }
         }
