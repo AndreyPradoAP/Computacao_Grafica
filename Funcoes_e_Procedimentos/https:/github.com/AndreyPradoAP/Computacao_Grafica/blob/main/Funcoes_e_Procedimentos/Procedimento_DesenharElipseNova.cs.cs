207ea888b0a5bb73d2221584243c22c6401895fa
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-08-11 12:08:05 -0300

    Create Procedimento_DesenharElipseNova.cs.cs

---
+++ Funcoes_e_Procedimentos/https:/github.com/AndreyPradoAP/Computacao_Grafica/blob/main/Funcoes_e_Procedimentos/Procedimento_DesenharElipseNova.cs.cs
@@ -0,0 +1,4 @@
+public void desenharCircunferenciaNova(PaintEventArgs e, Pen caneta, int Xcentro, int Ycentro, int largura, int altura)
+{
+  e.Graphics.DrawEllipse(caneta, Xcentro, Ycentro, largura, altura);
+}
