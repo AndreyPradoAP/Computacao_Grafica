a2fea232787f24b013dab1080fffebb5cac7bd1b
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2022-08-11 12:09:37 -0300

    Delete Funcoes_e_Procedimentos/https:/github.com/AndreyPradoAP/Computacao_Grafica/blob/main/Funcoes_e_Procedimentos directory

--- Funcoes_e_Procedimentos/https:/github.com/AndreyPradoAP/Computacao_Grafica/blob/main/Funcoes_e_Procedimentos/Procedimento_DesenharElipseNova.cs.cs
+++
@@ -1,4 +0,0 @@
-public void desenharCircunferenciaNova(PaintEventArgs e, Pen caneta, int Xcentro, int Ycentro, int largura, int altura)
-{
-  e.Graphics.DrawEllipse(caneta, Xcentro, Ycentro, largura, altura);
-}
